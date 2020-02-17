c447b0b3e15ead9d2abff6c24ed2f1ef1a3af9d4
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-17 00:47:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect { |call| call = call.capitalize + "!"}
-  planeteer_calls
+  
 end
 
 def long_planeteer_calls# code an argument here
